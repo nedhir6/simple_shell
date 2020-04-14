5a8f90b3f56eaea1472ff1b4d0d319ae42821b0f
karim bahri <karim-bahri-18@outlook.fr>
2020-04-14 21:58:15 +0000

    handle getline + EOF

--- input.c
+++ input.c
@@ -12,13 +12,13 @@
  */
 ssize_t input_shell(char **string)
 {
-	/*size_t nbRd = 0;*/
+	size_t nbRd = 0;
 	ssize_t n = 0;
 
 
 	write(STDOUT_FILENO, "$ ", 2);
-	/*n = getline(string, &nbRd, stdin);*/
-	n = _getline(string);
+	n = getline(string, &nbRd, stdin);
+	/*n = _getline(string);*/
 	if (n == -1)
 	{
 		NEWLINE;
